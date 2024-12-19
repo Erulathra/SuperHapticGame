5eb96535985012a4da8d7fda85f87eeb0b731a15
Szymon Świędrych <43550433+Erulathra@users.noreply.github.com>
2024-12-19 11:53:30 +0100

    Improve gyro handling

--- Assets/Scripts/GyroController.cs
+++ Assets/Scripts/GyroController.cs
@@ -1,16 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 public class GyroController : MonoBehaviour
 {
+    [field: SerializeField]
+    public Vector3 Sensivity { get; private set; } = new Vector3(0.005f, 0.005f, 0.005f);
+    
     void Start()
     {
         JSL.JslConnectDevices();
+
+        StartCoroutine(Calibrate());
+    }
+
+    IEnumerator Calibrate()
+    {
+        JSL.JslStartContinuousCalibration(0);
+        yield return new WaitForSeconds(1f);
+        JSL.JslPauseContinuousCalibration(0);
+        
+        Debug.Log($"Calibration End");
     }
 
     void Update()
     {
-        JSL.IMU_STATE State = JSL.JslGetIMUState(0);
+        JSL.IMU_STATE imuState = JSL.JslGetIMUState(0);
+        JSL.JOY_SHOCK_STATE simpleState = JSL.JslGetSimpleState(0);
         
-        Debug.Log($"Gyro State: ({State.gyroX}, {State.gyroY}, {State.gyroZ})");
+        if ((simpleState.buttons & (1 << JSL.ButtonMaskPlus)) != 0)
+        {
+            transform.rotation = Quaternion.identity;
+        }
+        
+        // Vector3 gyroEuler = new Vector3(0f, State.gyroZ, 0f);
+        Vector3 gyroEuler = new Vector3(imuState.gyroY * Sensivity.x, imuState.gyroZ * Sensivity.y, 0f);
+        Vector3 newRotation = transform.rotation.eulerAngles + gyroEuler;
+
+        transform.rotation = Quaternion.Euler(newRotation);
     }
 }
